95831572affa77142dd360161a011bb5be1f0263
Massimo Danieli <87365596+MassimoDanieli@users.noreply.github.com>
2023-07-17 18:03:38 +0100

    Update run_build.sh to run in macos x too

--- run_build.sh
+++ run_build.sh
@@ -4,12 +4,21 @@ export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 export GROUP_NAME=$(id -gn)
 
-mkdir -p /home/$USER_NAME/mod-user-files
-mkdir -p /home/$USER_NAME/mod-data
-mkdir -p /home/$USER_NAME/mod-pedalboards
+if [ "$(uname)" == "Darwin" ]; then
+    # Mac OS X platform
+    BASE_DIRECTORY="/Users"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # GNU/Linux platform
+    BASE_DIRECTORY="/home"
+fi
 
-docker build  --rm -f "mod-host/Dockerfile"  -t mod-host "mod-host"
-docker build  --rm -f "mod-plugin-builder/Dockerfile" --build-arg USERNAME=$USER_NAME --build-arg USERID=$USER_ID --build-arg GROUPID=$GROUP_ID -t mod-plugin-builder "mod-plugin-builder"
-docker build  --rm -f "mod-ui/Dockerfile" --build-arg USERNAME=$USER_NAME -t mod-ui "mod-ui" 
+mkdir -p $BASE_DIRECTORY/$USER_NAME/mod-user-files
+mkdir -p $BASE_DIRECTORY/$USER_NAME/mod-data
+mkdir -p $BASE_DIRECTORY/$USER_NAME/mod-pedalboards
 
-docker-compose up  --no-build mod-ui
+docker build --rm -f "mod-host/Dockerfile" -t mod-host "mod-host"
+docker build --rm -f "mod-plugin-builder/Dockerfile" --build-arg USERNAME=$USER_NAME --build-arg USERID=$USER_ID --build-arg GROUPID=$GROUP_ID -t mod-plugin-builder "mod-plugin-builder"
+docker build --rm -f "mod-ui/Dockerfile" --build-arg USERNAME=$USER_NAME -t mod-ui "mod-ui"
+
+docker-compose up --no-build mod-ui
+
