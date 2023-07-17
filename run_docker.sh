a9d608568d38073311701c5328d7100ae62990d2
Massimo Danieli <87365596+MassimoDanieli@users.noreply.github.com>
2023-07-17 18:02:07 +0100

    Update run_docker.sh to work in macos  x too

--- run_docker.sh
+++ run_docker.sh
@@ -4,10 +4,19 @@ export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 export GROUP_NAME=$(id -gn)
 
-mkdir -p /home/$USER_NAME/mod-user-files
-mkdir -p /home/$USER_NAME/mod-data
-mkdir -p /home/$USER_NAME/mod-pedalboards
+if [ "$(uname)" == "Darwin" ]; then
+    # Mac OS X platform
+    BASE_DIRECTORY="/Users"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # GNU/Linux platform
+    BASE_DIRECTORY="/home"
+fi
+
+mkdir -p $BASE_DIRECTORY/$USER_NAME/mod-user-files
+mkdir -p $BASE_DIRECTORY/$USER_NAME/mod-data
+mkdir -p $BASE_DIRECTORY/$USER_NAME/mod-pedalboards
 
 docker pull raidolo:mod-docker 
 
-docker-compose up  --no-build mod-ui
+docker-compose up --no-build mod-ui
+
